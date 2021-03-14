d4dc54a820af8d43379a0e1115db2a99c1494eec
Viktoryia Leshchyk <leshchik87@gmail.com>
2021-03-14 17:27:33 +0300

    feat: task solution complet

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
+ let result;
+ if (array && array.length)
+{
+  return  result = Math.min.apply(null, array);
+  
+}
+else {
   return 0;
 }
 
+}
+ 
+
 exports.max = function max (array) {
+  let result;
+  if (array && array.length)
+{
+ result = Math.max.apply(null, array);
+}
+else {
   return 0;
+}
+
+  return  result;
 }
 
 exports.avg = function avg (array) {
+  if (array && array.length)
+  {
+  return array.reduce((a,b) => a+b, 0)/array.length;
+}
+else {
   return 0;
+}
+
 }
